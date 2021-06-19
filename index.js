a9cc749a1c364c3077b8d08db28a23614dd701f3
nicomplished <72106498+nicomplished@users.noreply.github.com>
2021-06-18 19:03:27 -0500

    whoooole lootta changes

---
+++ index.js
@@ -0,0 +1,14 @@
+function showhide(id) {
+    var e = document.getElementById(id);
+    e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
